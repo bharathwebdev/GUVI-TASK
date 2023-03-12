56f848a5dfa9bbdac3fe8f1f11b1ab232dbac1a1
bharath <bharath18117886@gmail.com>
2023-03-12 11:11:18 +0530

    first commit

---
+++ js/login.js
@@ -0,0 +1,36 @@
+
+
+$(document).ready(function () {
+  // validating form inputs ;
+  $("#login-form").validate();
+  $("#alert").hide();
+
+  $("#login").click(function (e) {
+    console.log("login clicked");
+    if (document.getElementById("login-form").checkValidity()) {
+      console.log("im runing");
+      e.preventDefault();
+      $.ajax({
+        url: "php/login.php",
+        method: "post",
+        data: $("#login-form").serialize() + "&action=login",
+        success: function (response) {
+          console.log(response);
+          if (response != "Login Failed check your email and password !") {
+            // let responseJSON = JSON.parse(response);
+            // let {email,password} = responseJSON;
+            localStorage.setItem("Auth", response)
+
+            window.location = "profile.html";
+          } else {
+            $("#alert").show();
+            $("#result").html(response);
+          }
+        },
+      });
+    }
+    return true;
+  });
+});
+
+
